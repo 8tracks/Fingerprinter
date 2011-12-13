4fe65658dea16e17deb547c539e69e3ae60fffb7
Peter Bui <peter@paydrotalks.com>
2011-12-12 16:30:05 -0800

    Remove unused files

--- lastfmfpclient/src/main.cpp
+++ lastfmfpclient/src/main.cpp
@@ -23,17 +23,17 @@
 
 #include <sndfile.hh>
 
-#include "MP3_Source.h" // to decode mp3s
+//#include "MP3_Source.h" // to decode mp3s
 #include "HTTPClient.h" // for connection
 
-#include "Sha256File.h" // for SHA 256
-#include "mbid_mp3.h"   // for musicbrainz ID
+//#include "Sha256File.h" // for SHA 256
+//#include "mbid_mp3.h"   // for musicbrainz ID
 
 // taglib
-#include <fileref.h>
-#include <tag.h>
-#include <id3v2tag.h>
-#include <mpegfile.h>
+//#include <fileref.h>
+//#include <tag.h>
+//#include <id3v2tag.h>
+//#include <mpegfile.h>
 
 // stl
 #include <iostream>
@@ -115,87 +115,6 @@ string simpleTrim( const string& str )
 
 // -----------------------------------------------------------------------------
 
-void addEntry ( map<string, string>& urlParams, const string& key, const string& val )
-{
-  if ( key.empty() || val.empty() )
-    return;
-  if ( urlParams.find(key) != urlParams.end() )
-    return; // do not add something that was already there
-
-  urlParams[key] = simpleTrim(val);
-}
-
-// -----------------------------------------------------------------------------
-
-// gather some data from the (mp3) file
-void getFileInfo( const string& fileName, map<string, string>& urlParams, bool doTaglib = true )
-{
-
-  //////////////////////////////////////////////////////////////////////////
-  // Musicbrainz ID
-  char mbid_ch[MBID_BUFFER_SIZE];
-  if ( getMP3_MBID(fileName.c_str(), mbid_ch) != -1 )
-    urlParams["mbid"] = string(mbid_ch);
-
-  //////////////////////////////////////////////////////////////////////////
-  // Filename (optional)
-  size_t lastSlash = fileName.find_last_of(SLASH);
-  if ( lastSlash != string::npos )
-    urlParams["filename"] = fileName.substr(lastSlash+1);
-  else
-    urlParams["filename"] = fileName;
-
-  ///////////////////////////////////////////////////////////////////////////
-  // SHA256
-
-  const int SHA_SIZE = 32;
-  unsigned char sha256[SHA_SIZE]; // 32 bytes
-  Sha256File::getHash(fileName, sha256);
-
-  urlParams["sha256"] = Sha256File::toHexString(sha256, SHA_SIZE);
-
-  if ( !doTaglib )
-    return;
-
-  //////////////////////////////////////////////////////////////////////////
-  // ID3
-
-  try
-  {
-    TagLib::MPEG::File f(fileName.c_str());
-    if( f.isValid() && f.tag() )
-    {
-      TagLib::Tag* pTag = f.tag();
-
-      // artist
-      addEntry( urlParams, "artist", string(pTag->artist().toCString(true)) );
-
-      // album
-      addEntry( urlParams, "album", string(pTag->album().toCString(true)) );
-
-      // title
-      addEntry( urlParams, "track", string(pTag->title().toCString(true)) );
-
-      // track num
-      if ( pTag->track() > 0 )
-        addEntry( urlParams, "tracknum", toString(pTag->track()) );
-
-      // year
-      if ( pTag->year() > 0 )
-        addEntry( urlParams, "year", toString(pTag->year()) );
-
-      // genre
-      addEntry( urlParams, "genre", string(pTag->genre().toCString(true)) );
-    }
-  }
-  catch (const std::exception& /*e*/)
-  {
-    cerr << "WARNING: Taglib could not extract any information!" << endl;
-  }
-}
-
-// -----------------------------------------------------------------------------
-
 string fetchMetadata(int fpid, HTTPClient& client, bool justURL)
 {
   ostringstream oss;
@@ -233,63 +152,83 @@ int main(int argc, char* argv[])
       fileName = fileName.substr(lastSlash+1);
 
     cout << fileName << " (" << PUBLIC_CLIENT_NAME << ")\n"
-      << "A minimal fingerprint client, public release.\n"
-      << "Copyright (C) 2007-2010 by Last.fm (MIR) - Build: " << __DATE__ << " (" << __TIME__ << ")\n\n"
       << "Usage:\n"
-      << fileName << " [options] yourMp3File.mp3\n"
-      << "Available options:\n"
-      << " -nometadata: will only return the id of the fingerprint\n"
-      << " -url: will output the url of the metadata\n";
+      << fileName << " [options] yourMp3File.mp3\n\n"
+      << "REQUIRED OPTIONS:\n"
+      << " -duration: length of song in seconds\n"
+      << " -samplerate: something like 44100\n"
+      << " -channels: # of channels (1 or 2)\n";
     exit(0);
   }
 
-  string mp3FileName;
-  int duration, samplerate, bitrate, nchannels;
+  string wav_file_name;
+  int duration = -1, samplerate = -1, nchannels = -1;
+  string artist;
+  string filename;
+  string sha256;
+  string genre;
+  map<std::string, std::string> urlParams;
 
+  /* Old vars used in the mp3 version of this code */
   bool wantMetadata = true;
   bool justUrl = false;
 
-  bool doTagLib = true;
-  int forceDuration = 0;
+  bool debug = false;
 
   string serverName = FP_SERVER_NAME;
 
   // man, I am so used to boost::program_options that I suck at writing command line parsers now
 
+  cout << "argc: " << argc << endl;
   for(int i = 1; i < argc; ++i )
   {
     if ( argv[i][0] != '-' )
     {
-      mp3FileName = argv[i]; // assume it's the filename
+      wav_file_name = argv[i]; // assume it's the filename
       continue;
     }
 
     string arg(argv[i]);
 
-    if ( arg == "-nometadata" )
-      wantMetadata = false;
-    else if ( arg == "-url" )
-      justUrl = true;
-    else if ( arg == "-fplocation" && (i+1) < argc )
-    {
+    cout << "Argument: " << arg << endl;
 
-      ++i;
-      serverName = argv[i] + string("/fingerprint/query/");
+    if(arg == "-duration" && (i+1) < argc) {
+      duration = atoi(argv[++i]);
     }
-    else if ( arg == "-nometadatanotaglib" ) // hidden
-    {
-      wantMetadata = false;
-      doTagLib = false;
+    else if(arg == "-samplerate" && (i+1) < argc) {
+      samplerate = atoi(argv[++i]);
     }
-    else if ( arg == "-length" && (i+1) < argc )
-    {
-      ++i;
-      forceDuration = atoi(argv[i]);
-      if ( forceDuration == 0 )
-      {
-        cout << "Invalid length parameter for -lenght <" << argv[i] << ">\n";
-        exit(1);
-      }
+    else if(arg == "-channels" && (i+1) < argc) {
+      nchannels = atoi(argv[++i]);
+    }
+    // Metadata
+    else if(arg == "-artist" && (i+1) < argc) {
+      urlParams["artist"] = argv[++i];
+    }
+    else if(arg == "-album" && (i+1) < argc) {
+      urlParams["album"] = argv[++i];
+    }
+    else if(arg == "-track" && (i+1) < argc) {
+      urlParams["track"] = argv[++i];
+    }
+    else if(arg == "-tracknum" && (i+1) < argc) {
+      urlParams["tracknum"] = argv[++i];
+    }
+    else if(arg == "-year" && (i+1) < argc) {
+      urlParams["year"] = argv[++i];
+    }
+    else if(arg == "-genre" && (i+1) < argc) {
+      urlParams["genre"] = argv[++i];
+    }
+    else if(arg == "-filename" && (i+1) < argc) {
+      // This is the original mp3 file name if we have it
+      urlParams["filename"] = argv[++i];
+    }
+    else if(arg == "-sha" && (i+1) < argc) {
+      urlParams["sha256"] = argv[++i];
+    }
+    else if(arg == "-debug") {
+      debug = true;
     }
     else
     {
@@ -298,38 +237,47 @@ int main(int argc, char* argv[])
     }
   }
 
+
   //////////////////////////////////////////////////////////////////////////
 
   {
     // check if it exists
-    ifstream checkFile(mp3FileName.c_str(), ios::binary);
+    ifstream checkFile(wav_file_name.c_str(), ios::binary);
     if ( !checkFile.is_open() )
     {
-      cerr << "ERROR: Cannot find file <" << mp3FileName << ">!" << endl;
+      cerr << "ERROR: Cannot find file <" << wav_file_name << ">!" << endl;
       exit(1);
     }
   }
 
-  map<std::string, std::string> urlParams;
+  // Check for input - all these are required
+  {
+    if(duration <= 0) {
+      cerr << "ERROR: Duration must be > 0." << endl;
+      exit(1);
+    }
 
-  int duration, samplerate, bitrate, nchannels;
+    if(nchannels < 1) {
+      cerr << "ERROR: Channels must be 1 or more." << endl;
+      exit(1);
+    }
+
+    if (samplerate < 1) {
+      cerr << "ERROR: Samplerate must be > 0." << endl;
+      exit(1);
+    }
+  }
+
 
   // WARNING!!! This is absolutely mandatory!
   // If you don't specify the right duration you will not get the correct result!
-  if ( forceDuration > 0 )
-    urlParams["duration"] = toString(forceDuration);
-  else
-    urlParams["duration"] = toString(duration); // this is absolutely mandatory
-
+  // This has to be passed in.
+  urlParams["duration"]   = toString(duration); // this is absolutely mandatory
   urlParams["username"]   = PUBLIC_CLIENT_NAME; // replace with username if possible
   urlParams["samplerate"] = toString(samplerate);
 
-  // static short buffer [BUFFER_LEN] ;
-
   SndfileHandle infile ;
-  infile = SndfileHandle(mp3FileName);
-
-  // file.read (buffer, BUFFER_LEN) ;
+  infile = SndfileHandle(wav_file_name);
 
 
   // This will extract the fingerprint
@@ -341,9 +289,10 @@ int main(int argc, char* argv[])
   // wow, that's odd.. If I god directly with getVersion I get a strange warning with VS2005.. :P
   urlParams["fpversion"]  = toString( version );
 
-  // that's for the mp3
   // MP3_Source mp3Source;
   // the buffer can be any size, but FingerprintExtractor is happier (read: faster) with 2^x
+  //
+  // NOTE: This was left from the previous version of this code.
   const size_t PCMBufSize = 131072;
   short* pPCMBuffer = new short[PCMBufSize];
 
@@ -352,10 +301,7 @@ int main(int argc, char* argv[])
 
     for (;;) {
 
-      size_t read_data = infile.read(pPCMBuffer, PCMBufSize / 2);
-
-      // cout << *pPCMBuffer << endl;
-
+      size_t read_data = infile.read(pPCMBuffer, PCMBufSize / nchannels);
       if(fextr.process(pPCMBuffer, read_data)){
         break;
       }
@@ -367,31 +313,20 @@ int main(int argc, char* argv[])
 
 
     // Output URL PARAMS
-    // cout << "URL params:" << endl;
-    // map<std::string, std::string>::iterator i;
-    // for(i = urlParams.begin(); i != urlParams.end(); i++){
-    // cout << i->first << ": " << i->second << endl;
-    // }
-    // cout << endl;
-
-    map<std::string, std::string> my_url_params;
-    my_url_params["artist"]     = "Rod Stewart";
-    my_url_params["duration"]   = "326";
-    my_url_params["filename"]   = "Rod.mp3";
-    my_url_params["fpversion"]  = "1";
-    my_url_params["genre"]      = "Other";
-    my_url_params["samplerate"] = "44100";
-    my_url_params["sha256"]     = "fc4ef891e220ab751ee1328abaf1d40916ec1dba35dee4807a69001c9047f185";
-    my_url_params["username"]   = "fp client 1.6";
+    if(debug) {
+      cout << "URL params:" << endl;
+      map<std::string, std::string>::iterator i;
+      for(i = urlParams.begin(); i != urlParams.end(); i++){
+        cout << i->first << ": " << i->second << endl;
+      }
+      cout << endl;
+    }
 
     // send the fingerprint data, and get the fingerprint ID
     HTTPClient client;
-    // string c = client.postRawObj( serverName, urlParams,
-    // fpData.first, fpData.second,
-    // HTTP_POST_DATA_NAME, false );
-    string c = client.postRawObj( serverName, my_url_params,
-        fpData.first, fpData.second,
-        HTTP_POST_DATA_NAME, false );
+    string c = client.postRawObj(
+      serverName, urlParams, fpData.first, fpData.second, HTTP_POST_DATA_NAME, false);
+
     int fpid;
     istringstream iss(c);
     iss >> fpid;
